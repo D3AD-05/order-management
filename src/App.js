c8d19b9f3bcb23a0a4cb8063a5d9f0df4562a4d2
Dias <daisvalooran@gmail.com>
2024-02-05 09:35:35 +0530

    yi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import About from './pages/About';
-import Home from './pages/Home';
-import { Route, Routes } from "react-router-dom"
-import Login from './pages/Login/Login';
+import "./App.css";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import LoginComponent from "./pages/About";
 
 function App() {
   return (
     <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/about" element={<About />} />
-   
-  </Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<LoginComponent />} />
+    </Routes>
   );
 }
 
